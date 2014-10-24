0722148ba796c4870f062e1ea94e012a815327b5
Brent Axthelm <brent@bandpass.fm>
2014-10-24 19:23:21 -0400

    rails-api boilerplate. Command: rails-api new . --skip-sprockets --database=postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Vagrant::Application.config.secret_token = '2b5597bf1172b4482ffc5b0c49613f3a3f290515781cf4da4f3240228c48c617cff07595010ae5280978db02ddc1a115cade745dc095797c47d7d74e6556f26c'
